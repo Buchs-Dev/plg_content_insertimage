77fccc777a4324f3e73f9a7aaf018728e44a6253
René Alain Erichsen <re@ralain.com>
2024-06-25 11:13:05 +0200

    Add install script

---
+++ script.php
@@ -0,0 +1,76 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Installer\InstallerScript;
+
+/**
+ * Script file of Video Field plugin
+ */
+class plgContentInsertimageInstallerScript extends InstallerScript
+{
+    /**
+     * Method to install the extension
+     * $parent is the class calling this method
+     *
+     * @return void
+     */
+    function install($parent) 
+    {
+
+    }
+
+    /**
+     * Method to uninstall the extension
+     * $parent is the class calling this method
+     *
+     * @return void
+     */
+    function uninstall($parent) 
+    {
+
+    }
+
+    /**
+     * Method to update the extension
+     * $parent is the class calling this method
+     *
+     * @return void
+     */
+    function update($parent) 
+    {
+        // Get the plugin installation path for the site side
+        $path = $parent->getParent()->getPath('extension_site');
+
+        // Define the path to the "language" folder within the plugin's directory
+        $this->deleteFolders[] = $path . '/language';
+
+        // Call removeFiles to delete the folders listed in deleteFolders
+        $this->removeFiles($parent);
+    }
+
+    /**
+     * Method to run before an install/update/uninstall method
+     * $parent is the class calling this method
+     * $type is the type of change (install, update or discover_install)
+     *
+     * @return void
+     */
+    function preflight($type, $parent) 
+    {
+
+    }
+
+    /**
+     * Method to run after an install/update/uninstall method
+     * $parent is the class calling this method
+     * $type is the type of change (install, update or discover_install)
+     *
+     * @return void
+     */
+    function postflight($type, $parent) 
+    {
+
+    }
+}
