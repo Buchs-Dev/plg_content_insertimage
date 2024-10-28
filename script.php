7e9cdb28763ca41d31355cd4bf4c521f48cbe5b4
René Alain Erichsen <re@ralain.com>
2024-10-28 14:40:55 +0100

    Indent using spaces

--- script.php
+++ script.php
@@ -10,67 +10,67 @@ use Joomla\CMS\Installer\InstallerScript;
  */
 class plgContentInsertimageInstallerScript extends InstallerScript
 {
-	/**
-	 * Method to install the extension
-	 * $parent is the class calling this method
-	 *
-	 * @return void
-	 */
-	function install($parent)
-	{
+    /**
+     * Method to install the extension
+     * $parent is the class calling this method
+     *
+     * @return void
+     */
+    function install($parent)
+    {
 
-	}
+    }
 
-	/**
-	 * Method to uninstall the extension
-	 * $parent is the class calling this method
-	 *
-	 * @return void
-	 */
-	function uninstall($parent)
-	{
+    /**
+     * Method to uninstall the extension
+     * $parent is the class calling this method
+     *
+     * @return void
+     */
+    function uninstall($parent)
+    {
 
-	}
+    }
 
-	/**
-	 * Method to update the extension
-	 * $parent is the class calling this method
-	 *
-	 * @return void
-	 */
-	function update($parent)
-	{
-		// Get the plugin installation path for the site side
-		$path = JPATH_PLUGINS . '/content/insertimage';
+    /**
+     * Method to update the extension
+     * $parent is the class calling this method
+     *
+     * @return void
+     */
+    function update($parent)
+    {
+        // Get the plugin installation path for the site side
+        $path = JPATH_PLUGINS . '/content/insertimage';
 
-		// Define the path to the "language" folder within the plugin's directory
-		$this->deleteFolders[] = $path . '/language';
+        // Define the path to the "language" folder within the plugin's directory
+        $this->deleteFolders[] = $path . '/language';
 
-		// Call removeFiles to delete the folders listed in deleteFolders
-		$this->removeFiles($parent);
-	}
+        // Call removeFiles to delete the folders listed in deleteFolders
+        $this->removeFiles($parent);
+    }
 
-	/**
-	 * Method to run before an install/update/uninstall method
-	 * $parent is the class calling this method
-	 * $type is the type of change (install, update or discover_install)
-	 *
-	 * @return void
-	 */
-	function preflight($type, $parent)
-	{
+    /**
+     * Method to run before an install/update/uninstall method
+     * $parent is the class calling this method
+     * $type is the type of change (install, update or discover_install)
+     *
+     * @return void
+     */
+    function preflight($type, $parent)
+    {
 
-	}
+    }
 
-	/**
-	 * Method to run after an install/update/uninstall method
-	 * $parent is the class calling this method
-	 * $type is the type of change (install, update or discover_install)
-	 *
-	 * @return void
-	 */
-	function postflight($type, $parent)
-	{
+    /**
+     * Method to run after an install/update/uninstall method
+     * $parent is the class calling this method
+     * $type is the type of change (install, update or discover_install)
+     *
+     * @return void
+     */
+    function postflight($type, $parent)
+    {
 
-	}
+    }
 }
